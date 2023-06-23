85355e7ee78c50aadc55e315163a4af6b026ead5
Varun Manjunath <vama5032@colorado.edu>
2023-06-23 11:36:45 -0500

    Added Course Model

---
+++ models/course.py
@@ -0,0 +1,7 @@
+from app import db
+
+class Course(db.Model):
+    course_id = db.Column(db.Integer,primary_key=True)
+    course_name= db.Column(db.String)
+    course_author=db.Column(db.String)
+    course_endpoint=db.Column(db.String,unique=True,nullable=False)
